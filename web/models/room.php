90dc4e28cfd3dfee6e5ab386b3a5704e1074440f
santaclaushg <santaclaushg@gmail.com>
2020-05-22 08:02:19 +0700

    Change room password using room_id

--- web/models/room.php
+++ web/models/room.php
@@ -1,4 +1,5 @@
 <?php
+require_once('connection.php');
 class Room
 {
     public $room_password;
@@ -16,7 +17,7 @@ class Room
         $db = DB::getInstance();
 
         // check if exist $room_password -> then call error
-        $req_room_existed = $db->query("SELECT * FROM `room` as r WHERE r.room_password = {$room_password}");
+        $req_room_existed = $db->query("SELECT * FROM `room` as r WHERE r.room_id = {$room_password}");
         $item = $req_room_existed->fetchAll();
         if (count($item) > 0) {
             return $state["Room existed!"];
@@ -26,7 +27,7 @@ class Room
             return $state["Exceeds max player number!"];
         }
         // query to create Room
-        $db->exec("INSERT INTO room(room_password, player_number, is_closed) VALUES  
+        $db->exec("INSERT INTO room(room_id, player_number, is_closed) VALUES  
                         ( {$room_password},{$player_num},{$is_closed} )");
         return 1;
     }
@@ -34,7 +35,7 @@ class Room
     static function find($room_password, $state)
     {
         $db = DB::getInstance();
-        $req = $db->prepare('SELECT * FROM room WHERE room_password = :room_password');
+        $req = $db->prepare('SELECT * FROM room WHERE room_id = :room_password');
         $req->execute(array('room_password' => $room_password));
 
         $item = $req->fetch();
