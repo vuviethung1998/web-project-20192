a2ce43077c7bf025f86f6a92dcea9bcfea011791
vuviethung1998 <vuviethung.98.hust@gmail.com>
2020-06-09 10:47:35 +0700

    update

--- web/views/layouts/application.php
+++ web/views/layouts/application.php
@@ -4,15 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/intro_room_v3.css" />
-    <link rel="stylesheet" href="../../assets/css/create_room_v2.css" />
-    <link rel="stylesheet" href="../../assets/css/enter_room_v2.css" type="text/css">
+    <link rel="stylesheet" href="../../assets/css/skeleton.css" />
 </head>
 <body>
     <!-- Content -->
-    <div  class="bg">
-        <div class="title" >
-            <p class="customfont" href="#">Punishhhh</p>
-        </div>
+    <!-- <div  class="bg">
+        <div class="title" style="font-size: 10vw;">
+            Punishhhh
+        </div> -->
         <?= @$content ?>
     </div>
 <!-- Content end -->
