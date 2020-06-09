3105d26340a8f98c60c8d302d4392decf6cd273d
vuviethung1998 <vuviethung.98.hust@gmail.com>
2020-06-09 16:37:17 +0700

    update

--- web/views/layouts/application.php
+++ web/views/layouts/application.php
@@ -6,9 +6,9 @@
     <link rel="stylesheet" href="../../assets/css/intro_room_v3.css" type="text/css"/>
     <link rel="stylesheet" href="../../assets/css/create_room_v2.css" type="text/css"/>
     <link rel="stylesheet" href="../../assets/css/enter_room_v2.css" type="text/css">
-    <link rel="stylesheet" href="../../assets/css/skeleton.css" type="text/css"/>
     <link rel="stylesheet" href="../../assets/css/room.css" type="text/css"/>
     <link rel="stylesheet" href="../../assets/css/room_main.css" type="text/css"/>
+    <link rel="stylesheet" href="../../assets/css/skeleton.css" type="text/css"/>
 </head>
 <body>
     
