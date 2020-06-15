7f541974b10365473afa47258313416305dd5511
The Chinh <chinh1998@gmail.com>
2020-06-15 17:44:17 +0700

    Update part 2

--- web/views/rooms/enter_room.php
+++ web/views/rooms/enter_room.php
@@ -36,7 +36,7 @@
     $key =  array_search ($_SESSION['room_state'],  $state );
 
     if (!empty($key) && $key != 1) {
-      echo '<div id = "errorPopup" class = "pop-up">
+      echo '<div id = "errorPopup" class = "pop-up" style = "display:block">
       <div class = "pop-up-content">
         <div class = "pop-up-header">
           <h2 id="pop-up-title">ERROR</h2>
