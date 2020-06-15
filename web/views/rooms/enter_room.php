4f369ea1c02281951988491c48ff4d4502dfc313
The Chinh <chinh1998@gmail.com>
2020-06-15 15:47:04 +0700

    Update enter_room.php

--- web/views/rooms/enter_room.php
+++ web/views/rooms/enter_room.php
@@ -13,11 +13,6 @@
                         <input type="text" name="room_password" />
                     </td>
                 </tr>
-                <tr>
-                    <td style = "text-align: right;padding-bottom: 10px; padding-left: 200px"><input type = "checkbox" id = "checkbox-as-host" value = "yes"name = "checkbox" style = "width: 50px; height: 50px"></td>
-                    <td style="text-align: left; padding-bottom: 15px; padding-left: 30px" class="text-checkbox"> <p>Join room as Host<p></td>
-                </tr>
-
             </table>
             <button class="abutton" type="submit">enter</button>
         </form>
